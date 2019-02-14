666984b7b9b9cd20db54d451f10be71e5aa813b8
Matthijs van Polen <matthijs.v.polen@northwave.nl>
2019-02-14 11:53:57 +0100

    Now domains do not get extracted out of URLs.

--- src/core/operations/ExtractIOCs.mjs
+++ src/core/operations/ExtractIOCs.mjs
@@ -26,18 +26,11 @@ class ExtractIOCs extends Operation {
         this.inputType = "string";
         this.outputType = "string";
         this.args = [
-            /* Example arguments. See the project wiki for full details.
             {
-                name: "First arg",
-                type: "string",
-                value: "Don't Panic"
-            },
-            {
-                name: "Second arg",
-                type: "number",
-                value: 42
+                name: "Refang text first",
+                type: "boolean",
+                value: false
             }
-            */
         ];
     }
 
@@ -48,14 +41,20 @@ class ExtractIOCs extends Operation {
      */
     run(input, args) {
         // const [firstArg, secondArg] = args;
-        const DOMAIN_REGEX_EXCL = /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/ig;
+        const DOMAIN_REGEX_EXCL = /(?:(^|\s))(((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}(?=\s))/gmi;
         //const DOMAIN_REGEX_EXCL = /\s(?!:\/\/)[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}/ig;
         const IPv4 = /(?:(?:\d|[01]?\d\d|2[0-4]\d|25[0-5])\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d|\d)(?:\/\d{1,2})?/gi;
 
         let result = "DOMAINS\n=======\n";
-        result = result + search(input, DOMAIN_REGEX_EXCL) + "\n";
-        result = result + "URLS\n====\n" + search(input, URL_REGEX) + "\n";
-        result = result + "IPv4\n====\n" + search(input, IPv4) + "\n";
+
+        const temp = search(input, DOMAIN_REGEX_EXCL);
+        let arr = temp.split("\n");
+        arr = arr.map(item => item.trim());
+        arr = arr.filter(item => !!item);
+
+        result += arr.join("\n") + "\n\n";
+        result += "URLS\n====\n" + search(input, URL_REGEX) + "\n";
+        result += "IPv4\n====\n" + search(input, IPv4) + "\n";
 
         return result;
     }
